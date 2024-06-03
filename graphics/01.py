a5f8d9223b427ca2ec413427e4036a771feeca9f
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-06-04 01:34:03 +0500

    Create 01.py

---
+++ graphics/01.py
@@ -0,0 +1,14 @@
+import graphics
+import random
+
+CANVAS_WIDTH = 450
+CANVAS_HEIGHT = 300
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    for i in range(0, 5):
+        canvas.create_oval(i*20, i*20, i*20+20, i*20+20)
+    
+
+if __name__ == '__main__':
+    main()
